f64f970b0e4e1471dbfe8f5e8530e001f3fc3600
jackiege <jackie@jmac.local>
2017-02-09 17:15:40 +0800

    add db layer

--- src/controllers/contact_controller.ts
+++ src/controllers/contact_controller.ts
@@ -1,6 +1,8 @@
 import * as koa from 'koa'
+import { getContacts } from '../dal/contact_dal'
 
 const getContact = async (ctx: koa.Context, next) => {
+    getContacts();
     ctx.body = {
         id: '1',
         name: 'jackie'
---
+++ src/dal/contact_dal.ts
@@ -0,0 +1,41 @@
+import * as pg from 'pg'
+
+// create a config to configure both pooling behavior
+// and client options
+// note: all config is optional and the environment variables
+// will be read if the config is not present
+const config: pg.PoolConfig = {
+    user: 'postgres', //env var: PGUSER
+    database: 'test', //env var: PGDATABASE
+    password: 'jackie', //env var: PGPASSWORD
+    host: 'localhost', // Server hosting the postgres database
+    port: 5433, //env var: PGPORT
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+//this initializes a connection pool
+//it will keep idle connections open for a 30 seconds
+//and set a limit of maximum 10 idle clients
+
+function getContacts() {
+    const pool = new pg.Pool(config);
+    pool.connect((err, client, done) => {
+        //call `done()` to release the client back to the pool
+        done();
+        if (err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query('SELECT "Id", "Name" FROM public.contact').then(value => {
+            if (value.rowCount > 0) {
+                console.log(`returned ${value.rowCount} records`)
+                for (let row of value.rows) {
+                    console.log(`${row["Name"]}`)
+                }
+            }
+        });
+    })
+}
+
+export { getContacts }
